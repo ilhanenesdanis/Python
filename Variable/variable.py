494ccaa2a381ad79e140111254b7af0f7678ace2
İlhan Enes Daniş <danisenes3@gmail.com>
2025-01-05 21:25:05 +0300

    variable

--- Variable/variable.py
+++ Variable/variable.py
@@ -1,2 +1,27 @@
-
 print("Hello python")
+
+
+var1 = 10
+var2 = 15
+
+
+var3 = var1 + var2
+
+
+print(var3)
+
+
+
+s= "Hello python"
+
+variable_type=type(s)
+
+print(s)
+
+lengths=len(s)
+
+print(lengths)
+
+s_index=s[4]
+
+print(s_index)
