bf727b577907b2b0e64c83a5e7dc24e970e63cc5
İlhan Enes Daniş <danisenes3@gmail.com>
2025-01-11 23:38:48 +0300

    conditionals

---
+++ Conditionals/conditionals.py
@@ -0,0 +1,25 @@
+
+# if else statements
+
+# if else statements are used to execute a block of code if a condition is true. 
+# If the condition is false, another block of code can be executed.
+
+var1 =10
+var2 =20
+
+if(var1>var2):
+    print("var1 is greater than var2")
+elif(var1==var2):
+        print("var1 is equal to var2") 
+else:
+    print("var2 is greater than var1")
+
+lists=[1,2,3,4,5,7,8,9,10]
+
+if 6 in lists:
+    print("6 is in the list")
+else:
+    print("6 is not in the list")
+
+
+    
