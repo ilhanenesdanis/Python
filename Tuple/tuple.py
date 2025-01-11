f9665001ca4cae3e542bc77cc911661e33c44a3b
İlhan Enes Daniş <danisenes3@gmail.com>
2025-01-11 23:11:15 +0300

    tuple init

---
+++ Tuple/tuple.py
@@ -0,0 +1,10 @@
+#tuple 
+
+t=(1,2,3,4)
+
+print(t)
+
+
+print(t.count(5))# tuple içinde kaç tane 5 değeri olduğunu hesaplar
+
+print(t.index(3))# tuple içinde 3 değerinin kaçıncı indexte olduğunu hesaplar
