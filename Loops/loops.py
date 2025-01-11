2826fb4e285471021d3fd93a274481f7514cb448
İlhan Enes Daniş <danisenes3@gmail.com>
2025-01-11 23:44:42 +0300

    for loop

---
+++ Loops/loops.py
@@ -0,0 +1,16 @@
+#for loop
+
+for each in range(1,11):
+    print(each)
+
+
+lists=[1,2,4,56,7,3,13,44,53]
+
+sum_list=sum(lists)
+
+print(sum_list)
+
+count=0
+for each in lists:
+    count=count+each
+    print(count)
