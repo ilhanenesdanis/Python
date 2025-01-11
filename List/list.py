54fe6bdbf81abcf9b8d0767fa6b17e52bd50ae4e
İlhan Enes Daniş <danisenes3@gmail.com>
2025-01-11 23:08:51 +0300

    list init

---
+++ List/list.py
@@ -0,0 +1,28 @@
+##list 
+
+# List is a collection which is ordered and changeable. Allows duplicate members.
+
+list=[1,2,3,4,5]
+
+print(list)
+
+string_list=["one","two","three","four","five"]
+
+print(string_list)
+
+string_list.reverse()
+
+print(string_list)
+
+string_list.append(1)
+
+print(string_list)
+
+string_list.remove("one")
+
+print(string_list)
+
+string_list.sort()#exception will be thrown if list contains both string and integer
+
+
+print(string_list)
