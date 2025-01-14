a9c6c83aa9c6d63ca803d984242a522b68471111
İlhan Enes Daniş <danisenes3@gmail.com>
2025-01-14 21:09:37 +0300

    numpy basic operations

--- numpy/numpy_basics.py
+++ numpy/numpy_basics.py
@@ -1,38 +1,70 @@
 import numpy as np
 
 
-array =np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+# array =np.array([1,2,3,4,5,6,7,8,9,10,11,12])
 
 
-print(array.shape)
+# print(array.shape)
 
 
-a=array.reshape(3,4)
+# a=array.reshape(3,4)
 
+# print(a)
+# print("Shape:" ,a.shape)
+# print("Dimension:", a.ndim)
+# print("Data type:", a.dtype)
+# print("Size:", a.size)
+# print("Type:", type(a))
+
+# arra1=np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+# print(arra1.shape)
+
+# zeros= np.zeros((3,4))#allocation of zeros
+
+# zeros[0,0]=58
+# print(zeros)
+
+# np.ones((3,4))#allocation of ones
+
+# np.empty((2,3))#allocation of empty
+
+# c=np.arange(10,50,5)
+
+# print(c)
+
+# d=np.linspace(10,50,20)
+
+# print(d)
+
+
+#numpy basic operations
+a=np.array([1,2,3])
+b =np.array([4,5,6])
+
+print(a+b)
+print(a-b)
+print(a**2)
+
+print(np.sin(a))
+
+print(a<2)
+
+a=np.array([[1,2,3],[4,5,6]])
+b=np.array([[1,2,3],[4,5,6]])
+
+#element wise product
+print(a*b)
+
+a=np.random.random((5,5))
 print(a)
-print("Shape:" ,a.shape)
-print("Dimension:", a.ndim)
-print("Data type:", a.dtype)
-print("Size:", a.size)
-print("Type:", type(a))
 
-arra1=np.array([[1,2,3],[4,5,6],[7,8,9]])
 
-print(arra1.shape)
+print(a.sum())
+print(a.max())
+print(a.min())
 
-zeros= np.zeros((3,4))#allocation of zeros
+print(a.sum(axis=0))
 
-zeros[0,0]=58
-print(zeros)
-
-np.ones((3,4))#allocation of ones
-
-np.empty((2,3))#allocation of empty
-
-c=np.arange(10,50,5)
-
-print(c)
-
-d=np.linspace(10,50,20)
-
-print(d)
+print(np.sqrt(a))
+print(np.square(a))
